179a1684dc68dca4ebc4e2571f593e903ee950f2
codecheri <54028059+codecheri@users.noreply.github.com>
2020-04-23 23:46:47 +0200

    Use androidx

--- android/src/main/java/com/freshchat/consumer/sdk/react/RNFreshchatSdk.java
+++ android/src/main/java/com/freshchat/consumer/sdk/react/RNFreshchatSdk.java
@@ -6,9 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
+import androidx.support.annotation.NonNull;
+import androidx.support.annotation.Nullable;
+import androidx.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.facebook.react.bridge.Callback;
